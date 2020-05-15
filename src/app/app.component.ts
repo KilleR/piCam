4290c13e54d94220e0624caeaa5497924722fd4e
Richard Kille <richard.kille@pushdr.com>
2020-05-15 16:48:15 +0100

    added images!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'picSlice';
+
+  numbers = Array(24).fill(0).map((x, i) => i);
 }
